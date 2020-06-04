006d3112b89c2061ceec2240577898ac5ba4eee7
Owlspec3086 <ethical.worker3@gmail.com>
2020-06-04 11:36:56 -0400

    testing 1,2

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import * rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+it('renders a div with text')
